fb76cdcf0e9b4647d6e592da45deb61b26ff0beb
carlositaloo <carlos_italo@email.com>
2024-03-27 13:45:36 -0300

    OK

--- Automação Canva/main.py
+++ Automação Canva/main.py
@@ -1,24 +1,36 @@
-# python
-# from mouseinfo import mouseInfo
-# mouseInfo()
-# F6
-
 import os
 import pandas as pd
 import pyautogui
 import pyperclip
+from pynput import keyboard
+from time import sleep
 
 os.system('cls')
 
 path_clone = 'img/clone.png'
+path_texto = 'img/texto.png'
 arquivo_excel = 'arquivo.xlsx'
 litas_nomes = []
+
+# Variável global para controlar a execução do código
+execucao_cancelada = False
+
+def on_press(key):
+    global execucao_cancelada
+    if key == keyboard.Key.esc:
+        print("Execução cancelada.")
+        execucao_cancelada = True
+        return False  # Para a execução do listener
+
+# Criar um listener para a tecla "Esc"
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
 
 def ler_nomes_excel(arquivo):
     # Ler o arquivo Excel
     df = pd.read_excel(arquivo, header=None)
     # Pegar a primeira coluna (assumindo que é a coluna 0)
-    nomes = df.iloc[:, 3]
+    nomes = df.iloc[:, 4]
     # Remover linhas vazias e parar se encontrar alguma
     for nome in nomes:
         if pd.isna(nome):
@@ -29,22 +41,31 @@ def ler_nomes_excel(arquivo):
     return litas_nomes
 
 def criar_designs(nomes):
-    pyautogui.click(661,880)
+    if execucao_cancelada:
+        return
+    
+    pyautogui.click(697,880)
     pyautogui.PAUSE = 0.4
     nomes.sort(key=str.upper)
     for indice, nome in enumerate(reversed(nomes), start=1):
+        if execucao_cancelada:
+            break
         print(f"{indice}. {nome}")
         pyperclip.copy(nome)
         
-        pyautogui.click(1404,597)
+        # pyautogui.click(452,191)
         pyautogui.press('PageUp', presses=3)
         
         path = list(pyautogui.locateAllOnScreen(path_clone, confidence=0.9))
         x, y = pyautogui.center(path[0])
         pyautogui.click(x, y)
         
-        # pyautogui.doubleClick(931,321)
-        pyautogui.doubleClick(941,623)
+        pyautogui.click(452, 191)
+        pyautogui.press('PageUp', presses=3)
+        
+        path = list(pyautogui.locateAllOnScreen(path_texto, confidence=0.9))
+        x, y = pyautogui.center(path[1])
+        pyautogui.doubleClick(x,y)
         pyautogui.hotkey('ctrl', 'a')
         pyautogui.hotkey('ctrl', 'v')
     pyautogui.click(1404,597)
