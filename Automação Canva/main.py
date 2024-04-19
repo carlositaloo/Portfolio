d60d8b3e269cbbaf9e417e97cc2d82e15dc747d6
carlositaloo <carlos_italo@email.com>
2024-04-19 13:52:27 -0300

    OK

--- Automação Canva/main.py
+++ Automação Canva/main.py
@@ -7,18 +7,24 @@ from time import sleep
 
 os.system('cls')
 
-# Digite 1 para criar designs a partir de um design já existente ou 
+# Digite
+# 1 para criar designs a partir de um design já existente
 # 2 para alterar o texto de designs já existentes
-tipo_design = 2
+# 3 para alterar o texto de designs já existentes com dois textos
+tipo_design = 1
 
 path_clone = 'img/clone.png'
 path_texto = 'img/texto.png'
 path_texto1 = 'img/texto1.png'
 arquivo_excel = 'arquivo.xlsx'
-litas_nomes = []
+
+# Armazenar os nomes em uma lista
+lista_coluna1 = []
+lista_coluna2 = []
 
 # Variável global para controlar a execução do código
 execucao_cancelada = False
+
 
 def on_press(key):
     global execucao_cancelada
@@ -27,83 +33,126 @@ def on_press(key):
         execucao_cancelada = True
         return False  # Para a execução do listener
 
-# Criar um listener para a tecla "Esc"
-listener = keyboard.Listener(on_press=on_press) 
-listener.start()    
 
-def ler_nomes_excel(arquivo):   
+# Criar um listener para a tecla "Esc"
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+
+
+def ler_nomes_excel(arquivo):
     # Ler o arquivo Excel
     df = pd.read_excel(arquivo, header=None)    # Ler o arquivo Excel
     # Pegar a primeira coluna (assumindo que é a coluna 0)
-    nomes = df.iloc[:, 0]   # Alterar o número da coluna se necessário
-    # Remover linhas vazias e parar se encontrar alguma
-    for nome in nomes:  
-        if pd.isna(nome):   # Se a linha estiver vazia
+    coluna1 = df.iloc[:, 0]   # Alterar o número da coluna se necessário
+    if tipo_design == 3:
+        coluna2 = df.iloc[:, 1]   # Alterar o número da coluna se necessário
+        for nome2 in coluna2:
+            lista_coluna2.append(nome2)
+
+    for nome1 in coluna1:
+        if pd.isna(nome1):   # Se a linha estiver vazia
             print("Linha vazia encontrada, interrompendo a leitura.")
             break
-        litas_nomes.append(nome)
+        lista_coluna1.append(nome1)
 
-    return litas_nomes
+    return lista_coluna1, lista_coluna2
 
     # Essa função cria os designs a partir de um desing já existente, criando clones e alterando o texto
-def criar_designs(nomes):
+
+
+def criar_designs(coluna1):
     if execucao_cancelada:  # Verificar se a tecla ESC foi pressionada e cancelar a execução
         return
-    
-    pyautogui.click(697,880)    # Clicar no botão do navegador *Defina as coordenadas*
+
+    # Clicar no botão do navegador *Defina as coordenadas*
+    pyautogui.click(697, 880)  # Clicar no botão do navegador
     pyautogui.PAUSE = 0.4
-    nomes.sort(key=str.upper)   # Ordenar os nomes em ordem alfabética
-    for indice, nome in enumerate(reversed(nomes), start=1):    # Iterar sobre os nomes
+    coluna1.sort(key=str.upper)   # Ordenar a coluna1 em ordem alfabética
+    # Iterar sobre a coluna1
+    for indice, nome in enumerate(reversed(coluna1), start=1):
         if execucao_cancelada:  # Verificar se a tecla ESC foi pressionada e cancelar a execução
             break
         print(f"{indice}. {nome}")
         pyperclip.copy(nome)
-        
+
         # pyautogui.click(452,191)
         pyautogui.press('PageUp', presses=3)    # Subir a página
-        
-        path = list(pyautogui.locateAllOnScreen(path_clone, confidence=0.9))    # Procurar o botão de clone
-        x, y = pyautogui.center(path[0])    # Pegar o centro do botão e armazena em x e y
+        # Procurar o botão de clone
+        path = list(pyautogui.locateAllOnScreen(path_clone, confidence=0.9))
+        # Pegar o centro do botão e armazena em x e y
+        x, y = pyautogui.center(path[0])
         pyautogui.click(x, y)   # Clicar no botão de clone
-        
+
         pyautogui.click(452, 191)   # Clicar fora do layout
+        sleep(0.5)
         pyautogui.press('PageUp', presses=3)    # Subir a página
-        
-        path = list(pyautogui.locateAllOnScreen(path_texto, confidence=0.9))    # Procurar o campo de texto
+
+        # Procurar o campo de texto
+        path = list(pyautogui.locateAllOnScreen(path_texto, confidence=0.9))
         # Defina o índice do campo de texto* 0 primeiro campo, 1 segundo campo...
-        x, y = pyautogui.center(path[1])    # Pegar o centro do campo de texto e armazena em x e y
-        pyautogui.doubleClick(x,y)  # Clicar duas vezes no campo de texto
+        # Pegar o centro do campo de texto e armazena em x e y
+        x, y = pyautogui.center(path[1])
+        pyautogui.doubleClick(x, y)  # Clicar duas vezes no campo de texto
         pyautogui.hotkey('ctrl', 'a')   # Selecionar o texto
         pyautogui.hotkey('ctrl', 'v')   # Colar o texto
-    pyautogui.click(1404,597)   # Clicar fora do layout
+    pyautogui.click(1404, 597)   # Clicar fora do layout
 
     # Essa função apenas altera o texto dos designs já existentes
-def designs_existentes(nomes):
+
+
+def designs_existentes(coluna1, coluna2):
     if execucao_cancelada:  # Verificar se a tecla ESC foi pressionada e cancelar a execução
         return
-    
-    pyautogui.click(697,880)    # Clicar no botão do navegador *Defina as coordenadas*
+
+    # Clicar no botão do navegador *Defina as coordenadas*
+    pyautogui.click(697, 880)  # Clicar no botão do navegador
     pyautogui.PAUSE = 0.4
-    for indice, nome in enumerate(nomes, start=1):    # Iterar sobre os nomes
+    # Iterar sobre a coluna1
+    # for indice, (nome1, nome2) in enumerate(zip(coluna1, coluna2 or []), start=1):
+    for indice, nome1 in enumerate(coluna1, start=1):
+        if tipo_design == 3:
+            nome2 = coluna2[indice - 1]
+        else:
+            nome2 = ''
+
         if execucao_cancelada:  # Verificar se a tecla ESC foi pressionada e cancelar a execução
             break
-        print(f"{indice}. {nome}")
-        pyperclip.copy(nome)
-        
-        path = list(pyautogui.locateAllOnScreen(path_texto1, confidence=0.9))    # Procurar o campo de texto
+        print(f"{indice}. {nome1} {nome2}")
+        pyperclip.copy(nome1)
+
+        # Procurar o campo de texto
+        path = list(pyautogui.locateAllOnScreen(path_texto1, confidence=0.9))
         # Defina o índice do campo de texto* 0 primeiro campo, 1 segundo campo...
-        x, y = pyautogui.center(path[0])    # Pegar o centro do campo de texto e armazena em x e y 
-        pyautogui.doubleClick(x,y)  # Clicar duas vezes no campo de texto
+        # Pegar o centro do campo de texto e armazena em x e y
+        x, y = pyautogui.center(path[0])
+        pyautogui.doubleClick(x, y)  # Clicar duas vezes no campo de texto
         pyautogui.hotkey('ctrl', 'a')   # Selecionar o texto
         pyautogui.hotkey('ctrl', 'v')   # Colar o texto
         pyautogui.click(452, 191)   # Clicar fora do layout
-        
+
+        if tipo_design == 3:
+            pyperclip.copy(nome2)
+            # Procurar o campo de texto
+            path = list(pyautogui.locateAllOnScreen(
+                path_texto1, confidence=0.9))
+            # Defina o índice do campo de texto* 0 primeiro campo, 1 segundo campo...
+            # Pegar o centro do campo de texto e armazena em x e y
+            x, y = pyautogui.center(path[0])
+            pyautogui.doubleClick(x, y)  # Clicar duas vezes no campo de texto
+            pyautogui.hotkey('ctrl', 'a')   # Selecionar o texto
+            pyautogui.hotkey('ctrl', 'v')   # Colar o texto
+            pyautogui.click(452, 191)   # Clicar fora do layout
+
         pyautogui.press('PageDown', presses=1)    # Vai para o próximo design
-    pyautogui.click(1404,597)   # Clicar fora do layout
+    pyautogui.click(1404, 597)   # Clicar fora do layout
+
 
 # Executando o script e imprimindo os nomes
 ler_nomes_excel(arquivo_excel)
+
 if tipo_design == 1:
-    criar_designs(litas_nomes)
+    criar_designs(lista_coluna1)
 elif tipo_design == 2:
-    designs_existentes(litas_nomes)
+    designs_existentes(lista_coluna1, lista_coluna2)
+elif tipo_design == 3:
+    designs_existentes(lista_coluna1, lista_coluna2)
