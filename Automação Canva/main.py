18a7fd90fe3c6f2a5375273486a1cba0059724e6
carlositaloo <carlos_italo@email.com>
2024-06-13 11:30:10 -0300

    OK

--- Automação Canva/main.py
+++ Automação Canva/main.py
@@ -5,26 +5,24 @@ import pyperclip
 from pynput import keyboard
 from time import sleep
 
-os.system('cls')
-
 # Configurações
 fazer_clone = False     # Se deve criar clone
 num_textos = 1          # Quantos textos vai modificar
-num_coluna = 0          # 0 = default
-path_textos = ['img/texto.png', 'img/texto1.png', 'img/texto2.png', 'img/texto3.png' ]
+num_coluna = 0          # Coluna inicial no Excel '0 = Default'
+path_textos = ['img/texto.png', 'img/texto1.png', 'img/texto2.png', 'img/texto3.png']
 
-navegadorxy = 720, 883  # Coordenadas do botão do navegador
-foralayout = 1403, 187  # Coordenadas fora do layout
+navegadorxy = (720, 883)  # Coordenadas do botão do navegador
+foralayout = (1403, 187)  # Coordenadas fora do layout
 
 path_clone = 'img/clone.png'
 arquivo_excel = 'arquivo.xlsx'
+execucao_cancelada = False  # Variável global para controlar a execução do código
 
+# Limpa o terminal
+os.system('cls')
 
-# Armazenar os nomes em uma lista
+# Inicializa listas para armazenar os nomes
 listas_colunas = [[] for _ in range(num_textos + 1)]
-# Variável global para controlar a execução do código
-execucao_cancelada = False
-
 
 def on_press(key):
     global execucao_cancelada
@@ -33,74 +31,70 @@ def on_press(key):
         execucao_cancelada = True
         return False  # Para a execução do listener
 
-
 # Criar um listener para a tecla "Esc"
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
-
 def ler_nomes_excel(arquivo):
-    # Ler o arquivo Excel
+    """Lê os nomes do arquivo Excel e armazena em listas."""
     df = pd.read_excel(arquivo, header=None)
-    # Pegar as colunas necessárias
     for i in range(num_textos):
-        coluna = df.iloc[:, i+num_coluna]
+        coluna = df.iloc[:, i + num_coluna]
         for nome in coluna:
-            if pd.isna(nome):   # Se a linha estiver vazia
+            if pd.isna(nome):
                 print("Linha vazia encontrada, interrompendo a leitura.")
                 break
             listas_colunas[i].append(nome)
     return listas_colunas
 
-def fazer_designs(listas_colunas):
-    if execucao_cancelada:  # Verificar se a tecla ESC foi pressionada e cancelar a execução
-        return
+def clicar_botao_clone():
+    """Clica no botão de clone."""
+    pyautogui.click(foralayout)
+    pyautogui.press('PageUp', presses=3)
+    sleep(0.5)
+    path = list(pyautogui.locateAllOnScreen(path_clone, confidence=0.9))
+    if path:
+        x, y = pyautogui.center(path[0])
+        pyautogui.click(x, y)
+        sleep(1)
 
-    # Clicar no botão do navegador *Defina as coordenadas*
-    pyautogui.click(navegadorxy)  # Clicar no botão do navegador
+def processar_textos(listas_colunas):
+    """Itera sobre os textos e realiza as modificações necessárias."""
+    pyautogui.click(navegadorxy)
     pyautogui.PAUSE = 0.4
     
-    # Iterar sobre a colunas
     max_length = max(len(lista) for lista in listas_colunas)
-
+    
     for i in range(max_length):
-        items = []
-        if fazer_clone == True:
-            pyautogui.click(foralayout)
-            pyautogui.press('PageUp', presses=3)    # Subir a página
-            sleep(0.5)
-            path = list(pyautogui.locateAllOnScreen(path_clone, confidence=0.9))
-            # Pegar o centro do botão CLONE e armazena em x e y
-            x, y = pyautogui.center(path[0])
-            pyautogui.click(x, y)   # Clicar no botão de clone
-            sleep(1)
-            
+        if execucao_cancelada:
+            break
+
+        if fazer_clone:
+            clicar_botao_clone()
+
         for j, lista in enumerate(listas_colunas):
             if i < len(lista):
-                items.append(f"{lista[i]}")
-                pyperclip.copy(lista[i])
-                
-                if execucao_cancelada:  # Verificar se a tecla ESC foi pressionada e cancelar a execução
-                    break
-                
-                # Procurar o campo de texto
-                path = list(pyautogui.locateAllOnScreen(path_textos[j+num_coluna], confidence=0.9))
+                texto = lista[i]
+                pyperclip.copy(texto)
+                path = list(pyautogui.locateAllOnScreen(path_textos[j + num_coluna], confidence=0.9))
                 if path:
-                    # Pegar o centro do campo de texto e armazena em x e y
                     x, y = pyautogui.center(path[0])
-                    pyautogui.doubleClick(x, y)  # Clicar duas vezes no campo de texto
-                    pyautogui.hotkey('ctrl', 'a')   # Selecionar o texto
-                    pyautogui.hotkey('ctrl', 'v')   # Colar o texto
-                    pyautogui.click(foralayout)   # Clicar fora do layout
-        print(f"{i + 1}. {', '.join(items)}")
-        if execucao_cancelada:  # Verificar se a tecla ESC foi pressionada e cancelar a execução
+                    pyautogui.doubleClick(x, y)
+                    pyautogui.hotkey('ctrl', 'a')
+                    pyautogui.hotkey('ctrl', 'v')
+                    pyautogui.click(foralayout)
+        
+        if execucao_cancelada:
             break
-        if fazer_clone == False:
-            pyautogui.press('PageDown', presses=1)    # Vai para o próximo design
+        
+        print(f"{i + 1}. {', '.join(listas_colunas[j][i] for j in range(len(listas_colunas)) if i < len(listas_colunas[j]))}")
+        
+        if not fazer_clone:
+            pyautogui.press('PageDown', presses=1)
             sleep(0.5)
-    pyautogui.click(foralayout)   # Clicar fora do layout
 
+    pyautogui.click(foralayout)
 
-# Executando o script e imprimindo os nomes
+# Executando o script
 ler_nomes_excel(arquivo_excel)
-fazer_designs(listas_colunas)
+processar_textos(listas_colunas)
