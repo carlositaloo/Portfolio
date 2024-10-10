a833d4738397a8d4de5ac7520e75df948134b03c
carlositaloo <carlos_italo@email.com>
2024-10-10 08:46:49 -0300

    OK

--- MAPA DE NOTAS PARA O SIGE/MAIN.py
+++ MAPA DE NOTAS PARA O SIGE/MAIN.py
@@ -18,13 +18,15 @@ pyautogui.doubleClick(16, 109, duration=0.25)
 pyautogui.press('tab')
 
 # CONFIGURAÇÕES:
-canudo = True # Coloque True para 8º e 9º anos
+projetocamin = True # Coloque True para 8º e 9º anos
+adicionarcoluna = 0 # Use 2 para med
 # Lista das colunas a serem preenchidas
 Lista = {10: 'Art.', 28: 'Ciên.', 16: 'E.F.', 34: 'Relig.', 58: 'É.C.', 46: 'Geo.', 40: 'His.', 52: 'Ing.', 4: 'Port.', 22: 'Mat.', 67: 'P.I.', 70: 'Proj.C.'}
 
+Lista = {key + adicionarcoluna: value for key, value in Lista.items()}
 
 colunas = list(Lista.keys())
-if canudo == False:
+if projetocamin == False:
     colunas.pop()  # Remove a última chave
 
 def check_for_esc_key():
@@ -39,9 +41,11 @@ def converter_materia(colunas):
 
 materias = converter_materia(colunas)
 x = 0
+contador = 1  # Inicia o contador
 for linha in PAGINA.iter_rows(min_row=5):
     if linha[0].value is not None:
-        print(f' {" ".join(linha[0].value.split()[:2]):<20} ', end='|')
+        print(f'|{contador:>2}| {" ".join(linha[0].value.split()[:2]):<20} ', end='|')
+        contador += 1  # Incrementa o contador
         for coluna in colunas:
             check_for_esc_key()  # Verifique se a tecla Esc foi pressionada
             valor_celula = str(linha[coluna].value)
