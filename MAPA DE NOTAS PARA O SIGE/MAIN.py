818ef1ec7732163a23d40550544b5af7b82c6d60
carlositaloo <carlos_italo@email.com>
2025-03-06 09:41:01 -0300

    Mais tipos de configurações adicionadas

--- MAPA DE NOTAS PARA O SIGE/MAIN.py
+++ MAPA DE NOTAS PARA O SIGE/MAIN.py
@@ -8,7 +8,7 @@ import keyboard
 # mouseInfo()
 # F6
 
-os.system('cls')
+# os.system('cls')
 
 # PLANILHA = openpyxl.load_workbook('NOTAS.xlsx')
 PLANILHA = openpyxl.load_workbook('Exemplo.xlsx')
@@ -19,19 +19,24 @@ pyautogui.doubleClick(16, 109, duration=0.25)
 pyautogui.press('tab')
 
 # CONFIGURAÇÕES:
-projetocamin = True # Coloque True para 8º e 9º anos
-adicionarcoluna = 0 # Use 2 para adicionar a quantidade de cada key da lista.
+serie = 0 # 0 = 6º e 7º ano, 1 = 8º e 9º ano, 2 = EJA I e II, 3 = EJA III e IV
+coluna_inicial = 1 # Defalt: 1. 1ª coluna da planilha a ser preenchida
+linha_inicial = 5 # Defalt: 5. 1ª linha da planilha a ser preenchida
+
 # Lista das colunas a serem preenchidas
-# Lista = {10: 'Art.', 28: 'Ciên.', 16: 'E.F.', 34: 'Relig.', 58: 'É.C.', 46: 'Geo.', 40: 'His.', 52: 'Ing.', 4: 'Port.', 22: 'Mat.', 67: 'P.I.', 70: 'Proj.C.'}
+if serie == 0: #6º e 7º ano
+    Lista = {10: 'Art.', 28: 'Ciên.', 16: 'E.F.', 34: 'Relig.', 58: 'É.C.', 46: 'Geo.', 40: 'His.', 52: 'Ing.', 4: 'Port.', 22: 'Mat.', 64: 'P.I.', 70: 'Proj.C.'}
+elif serie == 1: #8º e 9º anos
+    Lista = {10: 'Art.', 28: 'Ciên.', 16: 'E.F.', 34: 'Relig.', 58: 'É.C.', 46: 'Geo.', 40: 'His.', 52: 'Ing.', 4: 'Port.', 22: 'Mat.', 64: 'P.I.'}
+elif serie == 2: #EJA I e II
+    Lista = {10: 'Art.', 28: 'Ciên.', 16: 'E.F.', 34: 'Relig.', 46: 'Geo.', 40: 'His.', 4: 'Port.', 22: 'Mat.'}
+elif serie == 3: #EJA III e IV
+    Lista = {10: 'Art.', 28: 'Ciên.', 16: 'E.F.', 34: 'Relig.', 46: 'Geo.', 40: 'His.', 52: 'Ing.', 4: 'Port.', 22: 'Mat.'}
 
-# EJAAAA:
-Lista = {10: 'Art.', 28: 'Ciên.', 16: 'E.F.', 34: 'Relig.', 46: 'Geo.', 40: 'His.', 52: 'Ing.', 4: 'Port.', 22: 'Mat.'}
-
+adicionarcoluna = 0 # Defalt: 0. Adiciona numeração ou subtrai da key da Lista anterior:
 Lista = {key + adicionarcoluna: value for key, value in Lista.items()}
 
 colunas = list(Lista.keys())
-if projetocamin == False:
-    colunas.pop()  # Remove a última chave
 
 def check_for_esc_key():
     if keyboard.is_pressed('esc'):
@@ -46,7 +51,7 @@ def converter_materia(colunas):
 materias = converter_materia(colunas)
 x = 0
 contador = 1  # Inicia o contador
-for linha in PAGINA.iter_rows(min_row=5):
+for linha in PAGINA.iter_rows(min_row=linha_inicial, min_col=coluna_inicial):
     if linha[0].value is not None:
         print(f'|{contador:>2}| {" ".join(linha[0].value.split()[:2]):<20} ', end='|')
         contador += 1  # Incrementa o contador
